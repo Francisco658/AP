7755383b590478696b0b0eee5c793d227303dc83
Francisco Claudino <franciscoclaudino@Francisco-MacBook-Pro.local>
2024-04-30 09:35:47 +0100

    deleted:    Source Code/PDF Files/Abdominal_Pelvic_Muscle_Training.pdf 	deleted:    Source Code/PDF Files/Exercise_Training.pdf 	deleted:    Source Code/PDF Files/Muscle_Strength_Jumping.pdf 	new file:   Source Code/PDF Files/Number_Sets.pdf 	deleted:    Source Code/PDF Temporal/TrainingMuscles.pdf 	new file:   Source Code/PDF Temporal/training_muscles.pdf 	new file:   Source Code/TXT Files/.DS_Store 	new file:   Source Code/TXT Files/training_muscles.txt 	deleted:    Source Code/TXT Files/treinar_musculos.txt 	new file:   Source Code/src/txt_converter.py
    	deleted:    Source Code/.DS_Store
    	deleted:    Source Code/TXT Files/.DS_Store

---
+++ Source Code/src/txt_converter.py
@@ -0,0 +1,20 @@
+def convert_to_lowercase(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            content = file.read()
+            lowercase_content = content.lower()
+        
+        with open(file_path, 'w') as file:
+            file.write(lowercase_content)
+        
+        return f"File '{file_path}' successfully converted to lowercase."
+    
+    except FileNotFoundError:
+        return f"File '{file_path}' not found."
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
+
+# Example usage:
+file_path = "../TXT Files/Training_Muscles.txt"
+result = convert_to_lowercase(file_path)
+print(result)
