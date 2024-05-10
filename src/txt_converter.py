2134736f89d4198701f41df409608d30372bd0af
Francisco Claudino <franciscoclaudino@Francisco-MacBook-Pro.local>
2024-05-10 18:34:06 +0100

    new file:   Final PDF Files/Workout_Planning.pdf 	new file:   PDF Files/Total_Number_of_Sets.pdf 	new file:   TXT Files/Workout_Planning.txt 	modified:   src/app.py 	modified:   src/txt_converter.py

--- src/txt_converter.py
+++ src/txt_converter.py
@@ -14,6 +14,6 @@ def convert_to_lowercase(file_path):
     except Exception as e:
         return f"An error occurred: {str(e)}"
 
-file_path = "../TXT Files/Warm_Up_2.txt"
+file_path = "../TXT Files/Workout_Planning.txt"
 result = convert_to_lowercase(file_path)
 print(result)
