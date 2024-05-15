a6bf8d06dd5a6544181855d315a6a88fe20673c4
Francisco Claudino <franciscoclaudino@Francisco-MacBook-Pro.local>
2024-05-15 17:24:52 +0100

    new file:   src/__pycache__/document_loader.cpython-311.pyc 	new file:   src/__pycache__/llm.cpython-311.pyc 	new file:   src/__pycache__/models.cpython-311.pyc 	modified:   src/llm.py 	deleted:    src/__pycache__/document_loader.cpython-311.pyc 	deleted:    src/__pycache__/llm.cpython-311.pyc 	deleted:    src/__pycache__/models.cpython-311.pyc

--- src/llm.py
+++ src/llm.py
@@ -8,13 +8,19 @@ from langchain_core.messages import get_buffer_string
 from langchain_core.prompts import format_document
 from langchain.prompts.prompt import PromptTemplate
 
-condense_question = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+#condense_question = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+#
+#Chat History:
+#{chat_history}
+#
+#Follow Up Input: {question}
+#Standalone question:"""
 
-Chat History:
-{chat_history}
-
-Follow Up Input: {question}
-Standalone question:"""
+condense_question = """Use the following pieces of context to answer the question at the end. 
+    If you don't know the answer, just say that you don't know, don't try to make up an answer.
+    {context}
+    Question: {question}
+    Helpful Answer:"""
 CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(condense_question)
 
 answer = """
@@ -46,7 +52,6 @@ def _combine_documents(
 memory = ConversationBufferMemory(
     return_messages=True, output_key="answer", input_key="question"
 )
-
 
 def getChatChain(llm, db):
     retriever = db.as_retriever(search_kwargs={"k": 10})
